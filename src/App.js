7e4ab4988a6bb221d2e3fe4a0f5731cc14ae40da
Musfirah.Ansari <musfirahfayyaz311@gmail.com>
2024-11-08 15:41:08 +0500

    Calculator Updated with dcent styling

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import Comp2 from './components/Comp2';
 
 import styled from 'styled-components'; //purely for UI
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
 const MeraButton = styled.button`
   color: white;
   background-color: indigo;
@@ -38,11 +41,15 @@ function App(){
 
     return (
       <React.Fragment>
-                <Calculator/>  
-               {/* <MeraButton>I am OK Button</MeraButton>
+                 <Calculator/>   
 
+                {/* <MeraButton>I am OK Button</MeraButton>
                <Comp2/> 
-               <Comp1/>  */}
+               <Comp1/>  
+               <p className='text-danger'>This is Para</p>
+               <p className='text-info'>This is Para</p>
+               <p className='text-warning'>This is Para</p> */}
+
 
          {/* <input type="text" onChange={handleChange} value={text}/>
         <button onClick={handleClick}>Add</button>
